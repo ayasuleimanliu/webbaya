c38fe0cbc6d0fa90fdf76790f690b394dabc4c66
ayasuleimanliu <32231119@students.liu.edu.lb>
2024-11-07 21:06:00 +0200

    Add files via upload

---
+++ IntroductionOfMeH1.java
@@ -0,0 +1,29 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package introduction.of.me.h1;
+
+/**
+ *
+ * @author FAOUR
+ */
+public class IntroductionOfMeH1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("**********************************");
+        System.out.println("*" +" Name: \"Aya Suleiman\"          *");
+        System.out.println("*" +"  Major: \"CSCI\"                 *");
+        System.out.println("*" +" Age: 19                       *");
+        System.out.println("*" +" City: \" Saida \"               *");
+        System.out.println("**********************************");
+        
+
+        // TODO code application logic here
+    }
+    
+}
